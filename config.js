b5b5e4451ed0075a6bd608f1cc3ee3eef6e0eb9e
Glucy-2 <Glucy-2@outlook.com>
2023-08-04 00:55:55 +0000

    Deploying to gh-pages from @ skimitmc/uptime-status@1180422135d5a8d0c53f3bcb970cdbb5d3942bb3 🚀

---
+++ config.js
@@ -0,0 +1,32 @@
+window.Config = {
+
+  // 显示标题
+  SiteTitle: 'skimit status',
+
+  // 副标题
+  SiteSubTitle: 'Data tested from US, may not be accurate',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1166553-f7b1a410dad36253ed0fb642',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://skimit.net/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/skimitmc/uptime-status'
+    },
+  ],
+};
